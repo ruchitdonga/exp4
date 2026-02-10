d1ef288bfebf75056a0884142d466bf08476ac09
ruchitdonga <ruchit.donga.btech2028@atlasskilltech.university>
2026-02-10 09:00:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,5 @@ import multiplication as m
 print("Addition Result = ", a.add(5, 6))
 print("Subtraction Result = ", s.sub(5, 6))
 print("Multiplication Result = ", m.mul(5, 6))
+
+# call division func
